3995f229284bbb7741a4e0df6e7a9b5ea880ff6f
Saddy <42200107+svddy@users.noreply.github.com>
2020-07-26 11:55:21 +1000

    SpeedCamera.py added - final version

---
+++ SpeedCamera.py
@@ -0,0 +1,47 @@
+import random
+import time
+
+# Implementing Azure connectivity.
+from azure.iot.device import IoTHubDeviceClient, Message
+
+# Azure Device Authentication.
+CONNECTION_STRING = "HostName=SpeedCameraHub.azure-devices.net;DeviceId=SpeedCameraDevice;SharedAccessKey=FT90biAeszHN/m5nGB3KcCW1S/M3SqdIWEor9uKmybg="
+
+# Output Message Formatting.
+SPEED = 100
+MSG_TXT = '{{"speed": {speed}}}'
+
+# Establish client connection with IoT Hub.
+def iothub_client_init():
+    client = IoTHubDeviceClient.create_from_connection_string(CONNECTION_STRING)
+    return client
+
+def iothub_client_telemetry_sample_run():
+
+    try:
+        client = iothub_client_init()
+        print ( "Connection established with IoT Hub. Vehicle speeds are now being monitored." )
+
+        while True:
+
+            # Speed simulation in effect.
+            speed = SPEED + (random.uniform(-20,20))
+            msg_txt_formatted = MSG_TXT.format(speed=speed)
+            message = Message(msg_txt_formatted)
+
+            # Identify detected speeding.
+            if speed > 108:
+              message.custom_properties["speedAlert"] = "true"
+            else:
+              message.custom_properties["speedAlert"] = "false"
+
+            print( "Speed Detected: {}".format(message) )
+            client.send_message(message)
+            time.sleep(random.uniform(0.2, 6.0))
+
+    except KeyboardInterrupt:
+        print ( "Communication halted with IoT Hub. (User Input)" )
+
+if __name__ == '__main__':
+    print ( "Speed Camera #1 Simulation" )
+    iothub_client_telemetry_sample_run()
